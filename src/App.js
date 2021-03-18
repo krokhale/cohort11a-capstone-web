f5c480ad7923688575d4cb98fbdc9b70e36ba0ff
krokhale <krishna.rokhale@gmail.com>
2021-03-17 17:32:51 -0700

    checking in

--- src/App.js
+++ src/App.js
@@ -1,24 +1,69 @@
 import logo from './logo.svg';
 import './App.css';
 
+import React, {useEffect, useState} from "react";
+
 function App() {
+
+  const [categories, setCategories] = useState([]);
+  const [selectedCategory, setSelectedCategory] = useState();
+
+
+  const fetchCategories = async () => {
+    console.log('this will fetch the categories');
+    let res = await fetch('http://localhost:3000/api/v1/categories');
+    let data = await res.json();
+    console.log(data);
+    setCategories(data);
+  };
+
+  useEffect(() => {
+    // this code will run only once on component mount
+    fetchCategories()
+  }, [])
+
+    useEffect(() => {
+        // this code is going to run whenever the selectedCategory changes
+        // fetchQuestions() TheTodo: fetch and show the questions
+    }, [selectedCategory])
+
+  // useEffect(() => {
+  //   // this code will run every time the someStateVariable changes
+  //   // this code will run every time var2 OR someStateVariable changes
+  //   // write code here that reloads the page as a side effect of var2 OR someStateVariable changing
+  // }, [someStateVariable, var2])
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="grid grid-cols-12">
+        <div className={'col-span-full border p-5'}>
+          <h1 className={'text-center text-3xl'}>App Title</h1>
+        </div>
+
+      </div>
+
+      <div className="grid grid-cols-12">
+        <div className={'col-span-full md:col-span-3 lg:col-span-2 border p-5'}>
+          {/*<h1 className={'text-center text-3xl'}>Categories list</h1>*/}
+            <ul>
+                {categories.map((category, index) => {
+                    return <li key={index} className={category.id == selectedCategory ? 'border p-5 cursor-pointer bg-gray-200' : 'border p-5 cursor-pointer'} onClick={() => {
+                        setSelectedCategory(category.id);
+                    }}>
+                        {category.name}
+                    </li>
+                })}
+            </ul>
+        </div>
+
+        <div className={'col-span-full md:col-span-9 lg:col-span-10 border p-5'}>
+          <h1 className={'text-center text-3xl'}>blank space</h1>
+        </div>
+
+      </div>
+
+      </>
   );
 }
 
