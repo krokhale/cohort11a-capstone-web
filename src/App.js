a62c89cedcee985f2473fb2746cc8cff5d57cd7d
krokhale <krishna.rokhale@gmail.com>
2021-03-22 17:28:34 -0700

    checking in

--- src/App.js
+++ src/App.js
@@ -7,14 +7,19 @@ function App() {
 
   const [categories, setCategories] = useState([]);
   const [selectedCategory, setSelectedCategory] = useState();
+  const [questions, setQuestions] = useState();
 
 
   const fetchCategories = async () => {
     console.log('this will fetch the categories');
-    let res = await fetch('http://localhost:3000/api/v1/categories');
-    let data = await res.json();
-    console.log(data);
-    setCategories(data);
+
+    // let res = await fetch('http://localhost:3000/api/v1/categories');
+    //   https://cohort11a-capstone-api.herokuapp.com
+      console.log(process.env.API_URL)
+    // let res = await fetch(`${process.env.API_URL}/api/v1/categories`);
+    // let data = await res.json();
+    // console.log(data);
+    // setCategories(data);
   };
 
   useEffect(() => {
@@ -34,6 +39,26 @@ function App() {
   // }, [someStateVariable, var2])
 
 
+    const fetchQuestionsForCategory = async (id) => {
+        console.log('fetch questions for this category id', id);
+        let res = await fetch(`http://localhost:3000/api/v1/categories/${id}/questions`);
+        let data = await res.json();
+        console.log(data);
+        setQuestions(data);
+        // setCategories(data);
+
+    };
+
+  const createNewQuestion = async () => {
+      console.log('create a question for the category id', selectedCategory)
+      // the usual fetch request / HINT : look up the stock API request
+      // 1. Make a POST request to create a question
+      // 2. Once the call is successful
+      // 3. Fetch the questions for a category again (reload the questions)
+      // 4. done!
+  };
+
+
   return (
     <>
       <div className="grid grid-cols-12">
@@ -45,11 +70,12 @@ function App() {
 
       <div className="grid grid-cols-12">
         <div className={'col-span-full md:col-span-3 lg:col-span-2 border p-5'}>
-          {/*<h1 className={'text-center text-3xl'}>Categories list</h1>*/}
+          <h1 className={'text-center text-3xl'}>Currently selected category is: {selectedCategory}</h1>
             <ul>
                 {categories.map((category, index) => {
                     return <li key={index} className={category.id == selectedCategory ? 'border p-5 cursor-pointer bg-gray-200' : 'border p-5 cursor-pointer'} onClick={() => {
                         setSelectedCategory(category.id);
+                        fetchQuestionsForCategory(category.id)
                     }}>
                         {category.name}
                     </li>
@@ -59,6 +85,23 @@ function App() {
 
         <div className={'col-span-full md:col-span-9 lg:col-span-10 border p-5'}>
           <h1 className={'text-center text-3xl'}>blank space</h1>
+
+            <button className={'border p-2 pl-4 pr-4 bg-gray-200'} onClick={createNewQuestion}>New Question</button>
+            <br/>
+            <br/>
+
+            <ul>
+                {questions && questions.map((question) => {
+                    return <li key={question.id}>
+                        {/*{question.questionTxt} - {question.Answers.length}*/}
+                        {question.questionTxt} {question.Answers.length >0 && <span>- <span>{question.Answers.length}</span></span>}
+
+                    </li>
+
+                })}
+            </ul>
+
+            {/*{questions && <p>{JSON.stringify(questions)}</p>}*/}
         </div>
 
       </div>
